004b87ffe3f7f8fcf3bbb0a0b8199456a8a92324
mrskunky69 <40913528+mrskunky69@users.noreply.github.com>
2024-07-31 21:53:36 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -6,3 +6,14 @@ AddEventHandler('rsg-bandits:server:robplayer', function()
 	local Player = RSGCore.Functions.GetPlayer(src)
 	Player.Functions.SetMoney('cash', 0)
 end)
+
+RegisterServerEvent('banditRaid:rewardPlayer')
+AddEventHandler('banditRaid:rewardPlayer', function()
+    local src = source
+    local Player = RSGCore.Functions.GetPlayer(src)
+    
+    if Player then
+        Player.Functions.AddMoney('cash', 50, "Bandit kill reward")
+        TriggerClientEvent('RSGCore:Notify', src, 'You received $50 for killing a bandit', 'success')
+    end
+end)
